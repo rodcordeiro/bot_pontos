27c8b5960a80b6996dfdede0ea1cb59162fe4948
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2023-11-14 17:22:52 -0300

    chore: logger options setup

--- src/core/logger/index.ts
+++ src/core/logger/index.ts
@@ -2,18 +2,27 @@ import pino from "pino";
 
 const Logger = pino({
   transport: {
-    targets: [
-      {
-        level: "info",
-        target: "pino-pretty",
-        options: { colorize: true },
-      },
-      {
-        level: "trace",
-        target: "pino/file",
-        options: { destination: "./pino-logger.log" },
-      },
-    ],
+    targets:
+      process.env.NODE_ENV === "development"
+        ? [
+            {
+              level: "info",
+              target: "pino-pretty",
+              options: { colorize: true },
+            },
+            {
+              level: "trace",
+              target: "pino/file",
+              options: { destination: "./pino-logger.log" },
+            },
+          ]
+        : [
+            {
+              level: "trace",
+              target: "pino/file",
+              options: { destination: "./pino-logger.log" },
+            },
+          ],
   },
   name: "bot",
 });
