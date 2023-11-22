dd01a5a96b0a57292723127369c00cd45665014a
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2023-11-22 16:40:33 -0300

    chore: review pontos command

--- src/commands/pontos/pontos.command.ts
+++ src/commands/pontos/pontos.command.ts
@@ -26,13 +26,13 @@ class Pontos extends BaseCommand implements IBaseCommand {
       const regex = new RegExp(/(\/pontos\s)(\d{0,2})/gim),
         validateBody = regex.test(message.body);
 
-      console.log({
-        linha,
-        regex,
-        teste: validateBody,
-        body: message!.body,
-        args,
-      });
+      // console.log({
+      //   linha,
+      //   regex,
+      //   teste: validateBody,
+      //   body: message!.body,
+      //   args,
+      // });
 
       if (!validateBody) {
         await message?.reply(
@@ -51,45 +51,53 @@ class Pontos extends BaseCommand implements IBaseCommand {
       const { data } = await api.get<API.APIResponse>("/");
       const handler = new Handler(data.pontos);
       const pontos = handler.filterBy(linha, "line_id");
-      // for await (const ponto of pontos) {
-      //   try {
-      //     const { reply } = await message.reply(Templates.PONTO(ponto));
-      //     console.log(ponto);
-      //     if (!!ponto.audio_link) {
-      //       const audio = await MessageMedia.fromUrl(
-      //         `https://raizes.rodrigocordeiro.com.br/pontos/${ponto.audio_link}`,
-      //         {
-      //           unsafeMime: true,
-      //         }
-      //       );
-      //       await reply("AUDIO", undefined, {
-      //         media: audio,
-      //       });
-      //     }
-      //   } catch (e) {
-      //     Logger.error(e);
-      //   }
-      // }
-      // pontos.map(async (ponto) => {});
-      const content = `*Pontos de ${pontos[0].line_description}*\n${pontos
-        .map(Templates.PONTO)
-        .join("-------------------------------\n")}\n${Templates.Footer(
-        pontos[0].line_description
-      )}`;
-      const sentMessage = await message?.reply(content);
-      if (pontos.some((ponto) => ponto.audio_link)) {
-        pontos
-          .filter((ponto) => ponto.audio_link)
-          .map(async (ponto) => {
-            const audio = await MessageMedia.fromUrl(
+      for await (const ponto of pontos) {
+        try {
+          if (!!ponto.audio_link) {
+            const msg = await message.reply(Templates.PONTO(ponto));
+            Logger.info(
               `https://raizes.rodrigocordeiro.com.br/pontos/${ponto.audio_link}`
             );
-            await sentMessage?.reply("", undefined, {
+            const audio = await MessageMedia.fromUrl(
+              `https://raizes.rodrigocordeiro.com.br/pontos/${ponto.audio_link}`,
+              {
+                unsafeMime: true,
+              }
+            );
+            await msg.reply("AUDIO", undefined, {
               media: audio,
             });
-          });
+          } else {
+            await message.reply(Templates.PONTO(ponto));
+          }
+        } catch (e) {
+          Logger.error(e);
+        }
       }
-      return;
+      // pontos.map(async (ponto) => {});
+      // const content = `*Pontos de ${pontos[0].line_description}*\n${pontos
+      //   .map(Templates.PONTO)
+      //   .join("-------------------------------\n")}\n${Templates.Footer(
+      //   pontos[0].line_description
+      // )}`;
+      // const sentMessage = await message?.reply(content);
+      // if (pontos.some((ponto) => ponto.audio_link)) {
+      //   pontos
+      //     .filter((ponto) => ponto.audio_link)
+      //     .map(async (ponto) => {
+      //       console.log(ponto.audio_link);
+      //       const audio = await MessageMedia.fromUrl(
+      //         `https://raizes.rodrigocordeiro.com.br/pontos/${ponto.audio_link}`
+      //       );
+
+      //       if (audio.filesize) {
+      //         await sentMessage?.reply("", undefined, {
+      //           media: audio,
+      //         });
+      //       }
+      //     });
+      // }
+      // return;
     } catch (e) {
       Logger.error(e);
       await message.reply(
