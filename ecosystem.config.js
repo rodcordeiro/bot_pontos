2b0e317fb3de95d2949453c086df1169a862cc41
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-03-18 16:37:07 -0300

    ci: review docker compose files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'bot_whats',
       script: 'npm run start:prod',
       // cron_restart: '0 7 * * *',
-      max_memory_restart: '10M',
+      max_memory_restart: '300M',
       stop_exit_codes: [0],
       exp_backoff_restart_delay: 100,
     },
