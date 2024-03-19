5cdc8522b80646f0dd5bcbcdece37818c251f3e7
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-03-19 19:25:30 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
       name: 'bot_whats',
       script: 'npm run start:prod',
       // cron_restart: '0 7 * * *',
-      max_memory_restart: '300M',
       stop_exit_codes: [0],
       exp_backoff_restart_delay: 100,
     },
